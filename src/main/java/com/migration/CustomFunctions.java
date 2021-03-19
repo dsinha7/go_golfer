a828ea8f39b3f4f97147f37fd16acd9819592a26
dsinha <dsinha@gmail.com>
2021-03-19 10:53:26 -0400

    java file

--- src/main/java/com/migration/CustomFunctions.java
+++ src/main/java/com/migration/CustomFunctions.java
@@ -6,7 +6,7 @@ package com.migration;
 public abstract class CustomFunctions {
 
     private static Logger logger = Logger.getLogger(CustomFunctions.class.getName() );
-
+    /*
     public static BigInteger farmFingerPrint(java.math.BigInteger id) {
         return id.multiply(BigInteger.valueOf(34));
     }
@@ -14,13 +14,15 @@ public abstract class CustomFunctions {
         return id.toString();
     }
 
-    public static String bitReverse(Integer id) {
-        return String.valueOf(Integer.reverse(id));
-    }
+     */
 
     public static String bitReverse(Long id) {
         return String.valueOf(Long.reverse(id));
     }
 
+    public static void main(String[] args) {
+        System.out.println(bitReverse(-4611686018427387904L));
+        System.out.println(bitReverse(-2305843009213693952L));
+    }
 
 }
